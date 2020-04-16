35326546de7e1c6e58ae712b4f13777508fb8598
chrisrodg29 <61160592+chrisrodg29@users.noreply.github.com>
2020-04-16 14:41:00 -0400

    added validation to profile-contact component

--- src/app/components/profile-contact/profile-contact.component.ts
+++ src/app/components/profile-contact/profile-contact.component.ts
@@ -10,17 +10,26 @@ import { User } from 'src/app/models/user';
 })
 export class ProfileContactComponent implements OnInit {
 
-  profileObject : User;
+  profileObject: User;
   currentUser: any = '';
   firstName: string;
   lastName: string;
   email: string;
   phone: string;
-  success :string;
+  success: string;
+
+  // Form Error messages
+  firstNameError: string;
+  lastNameError: string;
+  emailError: string;
+  phoneNumberError: string;
+  serverResponseError: string;
+
+
   constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
-    this.currentUser = this.userService.getUserById2(sessionStorage.getItem("userid")).subscribe((response)=>{
+    this.currentUser = this.userService.getUserById2(sessionStorage.getItem('userid')).subscribe((response) => {
       this.profileObject = response;
 
       this.firstName = this.profileObject.firstName;
@@ -29,17 +38,101 @@ export class ProfileContactComponent implements OnInit {
       this.phone = this.profileObject.phoneNumber;
 
     });
-    
+
   }
 
-  updatesContactInfo(){
+  updatesContactInfo() {
+    this.clearMessages();
     this.profileObject.firstName = this.firstName;
     this.profileObject.lastName = this.lastName;
     this.profileObject.email = this.email;
     this.profileObject.phoneNumber = this.phone;
+    const passValidation = this.validateFields();
+    if (passValidation) {
+      this.userService.updateUserInfo(this.profileObject).subscribe(
+        resp => {
+          if (resp === null) {
+            this.serverResponseError = 'Update Unsuccessful';
+          } else {
+            this.success = 'Updated Successfully!';
+          }
+        }
+      );
+    }
+  }
 
-    this.userService.updateUserInfo(this.profileObject);
-    this.success = "Updated Successfully!";
+  clearMessages() {
+    this.firstNameError = '';
+    this.lastNameError = '';
+    this.phoneNumberError = '';
+    this.emailError = '';
+    this.serverResponseError = '';
+    this.success = '';
+  }
+
+  validateFields(): boolean {
+    let i = 0;
+    if (this.profileObject.firstName === '') {
+      this.firstNameError = 'First name field required';
+      i = 1;
+    } else if (this.profileObject.firstName.length > 30) {
+      this.firstNameError = 'First name cannot be more than 30 characters in length';
+      i = 1;
+    } else if (!(this.validateName(this.profileObject.firstName))) {
+      this.firstNameError = 'First name allows only 1 space or hyphen and no illegal characters';
+      i = 1;
+    }
+
+    if (this.profileObject.lastName === '') {
+      this.lastNameError = 'Last name field required';
+      i = 1;
+    } else if (this.profileObject.lastName.length > 30) {
+      this.firstNameError = 'Last name cannot be more than 30 characters in length';
+      i = 1;
+    } else if (!(this.validateName(this.profileObject.lastName))) {
+      this.firstNameError = 'Last name allows only 1 space or hyphen and no illegal characters';
+      i = 1;
+    }
+
+    if (this.profileObject.phoneNumber === '') {
+      this.phoneNumberError = 'Phone number field required';
+      i = 1;
+    } else if (!(this.validatePhoneNumber(this.profileObject.phoneNumber))) {
+      this.phoneNumberError = 'Invalid Phone Number';
+      i = 1;
+    }
+
+    if (this.profileObject.email === '') {
+      this.emailError = 'Email field required';
+      i = 1;
+    } else if (!(this.validateEmail(this.profileObject.email))) {
+      this.emailError = 'Invalid email';
+      i = 1;
+    }
+
+    if (i === 1) {
+      return false;
+    } else {
+      return true;
+    }
+
+  }
+
+  // RegEx functions
+
+  validateName(name) {
+    const re = new RegExp('^[a-zA-Z\\u00C0-\\u017F]+[- ]?[a-zA-Z\\u00C0-\\u017F]+$');
+    return re.test(name);
+  }
+
+  validatePhoneNumber(num) {
+    const re = new RegExp('^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$');
+    return re.test(num);
+  }
+
+  validateEmail(email) {
+    const re = new RegExp('^\\w+\\.?\\w+@\\w+\\.[a-zA-Z]{2,4}$');
+    return re.test(email);
   }
 
 
