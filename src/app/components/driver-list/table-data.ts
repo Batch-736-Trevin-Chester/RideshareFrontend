300c4f640aa3790ef2d59c064d4d9843011a227d
Jim Doan <jimdoan4@gmail.com>
2020-04-17 15:32:47 -0400

    adding sort, filter, and pagination code. Using dummy data

---
+++ src/app/components/driver-list/table-data.ts
@@ -0,0 +1,68 @@
+export const TableData: Array<any> = [
+  {
+    'name': 'Victoria Cantrell',
+    'distance': '2',
+    'time': '3:00',
+    'viewInfo': '0839'
+  }, {
+    'name': 'James Wheeler',
+    'distance': '5',
+    'time': '5:00',
+    'viewInfo': '0839'
+  }, {
+    'name': 'Yony Sam',
+    'distance': '6',
+    'time': '1:00',
+    'viewInfo': '0839'
+
+  }, {
+    'name': 'Amber Ton',
+    'distance': '1',
+    'time': '4:00',
+    'viewInfo': '0839'
+  }, {
+  'name': 'Victoria Cantrell',
+  'distance': '2',
+  'time': '3:00',
+  'viewInfo': '0839'
+}, {
+  'name': 'James Wheeler',
+  'distance': '5',
+  'time': '5:00',
+  'viewInfo': '0839'
+}, {
+  'name': 'Yony Sam',
+  'distance': '6',
+  'time': '1:00',
+  'viewInfo': '0839'
+
+}, {
+  'name': 'Amber Ton',
+  'distance': '1',
+  'time': '4:00',
+  'viewInfo': '0839'
+
+}, {
+  'name': 'Victoria Cantrell',
+  'distance': '2',
+  'time': '3:00',
+  'viewInfo': '0839'
+}, {
+  'name': 'James Wheeler',
+  'distance': '5',
+  'time': '5:00',
+  'viewInfo': '0839'
+}, {
+  'name': 'Yony Sam',
+  'distance': '6',
+  'time': '1:00',
+  'viewInfo': '0839'
+
+}, {
+  'name': 'Amber Ton',
+  'distance': '1',
+  'time': '4:00',
+  'viewInfo': '0839'
+
+}
+];
